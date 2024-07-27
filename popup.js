0fcd592472eb42feb6a8632fb4715a0c6b11a79e
eavargasf <152314586+eavargasf@users.noreply.github.com>
2024-07-27 12:06:01 +0200

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.getElementById('go-button').addEventListener('click', () => {
+  const accessCode = document.getElementById('access-code').value;
+  const secretCode = 'AI1532';
+
+  if (accessCode === secretCode) {
+    chrome.storage.sync.set({notificationsEnabled: true}, () => {
+      document.getElementById('message').innerText = 'Notifications activated!';
+    });
+  } else {
+    document.getElementById('message').innerText = 'Invalid code. Please purchase the product to activate.';
+  }
+});
