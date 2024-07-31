377940dcd9bbf0613f06cbf11a036d9c7f703709
eavargasf <152314586+eavargasf@users.noreply.github.com>
2024-07-31 15:24:14 +0200

    Update main.js

--- myapp/main.js
+++ myapp/main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const errorDiv = document.getElementById('registration-error');
         errorDiv.textContent = '';
 
-        axios.post('https://the-uncuffed.online/login-registration', {
+        axios.post('https://the-uncuffed.online/', {
             name,
             email,
             password,
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const errorDiv = document.getElementById('login-error');
         errorDiv.textContent = '';
 
-        axios.post('https://the-uncuffed.online/login-registration', {
+        axios.post('https://the-uncuffed.online/', {
             email,
             password
         })
