3c82885d17bf452ec6eca090ae4830e1f1c90a03
eavargasf <152314586+eavargasf@users.noreply.github.com>
2024-07-31 12:15:54 +0200

    Update server.js

--- myapp/server.js
+++ myapp/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://the-uncuffed.online' // Update this to your frontend domain
+    origin: 'https://the-uncuffed.online' // Ensure this is your frontend domain
 }));
 
 const AIRTABLE_API_URL = 'https://api.airtable.com/v0/appFZBJefIOmr86zR/Registrations';
