1b66d59d27c3c9a5b8fd888a6c024b027912b294
eavargasf <152314586+eavargasf@users.noreply.github.com>
2024-08-03 09:32:16 +0000

    wow17

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
